e67aa7fe2b2c27082f86cf5be443130e47cc6034
RealistikDash <realistikdash@gmail.com>
2024-01-18 01:13:46 +0000

    add: web server

--- src/main/java/com/realistikosu/Main.java
+++ src/main/java/com/realistikosu/Main.java
@@ -2,12 +2,17 @@ package com.realistikosu;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.realistikosu.bancho.web.BanchoController;
+
+@SpringBootApplication
 public class Main {
     private static final Logger logger = LogManager.getLogger("HelloWorld");
 
     public static void main(String[] args) {
         logger.error("Hiya!");
-        final int a = 2;
+        SpringApplication.run(BanchoController.class);
     }
 }
