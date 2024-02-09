a7402c5dafbc6c964c9a6cdf764524be95d2d6be
RealistikDash <realistikdash@gmail.com>
2024-02-09 01:55:20 +0000

    add: random reflection tests

--- src/main/java/com/realistikosu/bancho/protocol/structures/SerialisablePacket.java
+++ src/main/java/com/realistikosu/bancho/protocol/structures/SerialisablePacket.java
@@ -3,18 +3,23 @@ package com.realistikosu.bancho.protocol.structures;
 import com.realistikosu.bancho.protocol.io.Writer;
 import com.realistikosu.bancho.protocol.io.Reader;
 import com.realistikosu.osu.PacketId;
+import com.squareup.javapoet.MethodSpec;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 
 /**
  * An abstract class defining the API for a packet structure.
  */
 public abstract class SerialisablePacket {
+    public SerialisablePacket() {
+        MethodSpec
+    }
     /**
      * Deserialises a byte stream into the packet form.
      * @param reader - A reader with the header read.
      */
-    public abstract SerialisablePacket read(Reader reader) throws IOException;
+    public abstract void read(Reader reader) throws IOException;
 
     /**
      * Writes the given packet with its data into a writer object.
@@ -38,18 +43,54 @@ public abstract class SerialisablePacket {
     public abstract PacketId packetId();
 
     /**
-     * Calculates the minimum size the packet body can occupy.
-     * @return - The minimum size in bytes.
-     */
-    public static int minimumLength() {
-
-    }
-
-    /**
      * Computes the current size of the packet body as if it were written.
      * @return - The packet body
      */
     public int currentLength() {
+        Field[] fields = this.getClass().getDeclaredFields();
+        int length = 0;
 
+        for (Field f : fields) {
+            Class type = f.getType();
+
+            // I tried making this a switch statement, but it didn't work.
+            if (type == boolean.class)
+                ++length;
+            else if (type == byte.class)
+                ++length;
+            else if (type == short.class)
+                length += 2;
+            else if (type == int.class)
+                length += 4;
+            else if (type == long.class)
+                length += 8;
+            else if (type == String.class) {
+                String writtenString = null;
+                try {
+                    writtenString = (String)f.get(this);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+                double stringLength = (double)writtenString.length();
+
+                // Uleb header size. Borrowed from https://github.com/infernalfire72/bancho-protocol
+                length += (int)Math.floor(Math.log(stringLength / 7d) / Math.log(2) + 2d);
+                length += (int) stringLength;
+            } else if (type == int[].class) {
+                int[] writtenList = new int[0];
+                try {
+                    writtenList = (int[])f.get(this);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+
+                // i16 prefix
+                length += 2;
+                // i32 list
+                length += writtenList.length * 4;
+            }
+        }
+
+        return length;
     }
 }
